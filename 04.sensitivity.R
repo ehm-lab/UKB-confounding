1677c9e0f22a22b1ee994af23f5f93a89c1e8aa0
Jappo Vanoli <jacopo.vanna@gmail.com>
2024-01-18 18:03:47 +0000

    Update 04.sensitivity.R

--- 04.sensitivity.R
+++ 04.sensitivity.R
@@ -12,7 +12,7 @@ resens <- list()
 # FIT MODEL WITH STRATA
 fmod <- funfmod()
 resens$strata <- coxph(fmod, data=fulldata, ties="efron") |> 
-  ci.exp(mod, subset="pm25_07", ctr.mat=matrix(10))
+  ci.exp(subset="pm25_07", ctr.mat=matrix(10))
 
 # FIT MODEL WITH WITH DUMMY VARIABLE
 fmod <- funfmod(strata=c("sex","birthyear"), 
@@ -48,3 +48,71 @@ rownames(modres) <- names(resens)
 
 # SAVE
 saveRDS(modres, file="temp/modresens.RDS")
+
+
+
+
+################################################################################
+# RUN SENSITIVITY MODEL FOR INCLUSION OF LONDON AND GLASGOW
+################################################################################
+
+# OBJECTS TO STORE THE RESULTS
+resens <- list()
+
+# FIT THE MAIN MODEL
+fmod <- funfmod()
+mod <- coxph(fmod, data=fulldata, ties="efron")
+
+# STORE THE RESULT (HR FOR A 10-UNIT INCREASE IN PM25)
+resens$main <- ci.exp(mod, subset="pm25_07", ctr.mat=matrix(10))
+
+# FIT MODEL WITH STRATA(CENTRE) WITHOUT LONDON 
+fmod <- funfmod()
+resens$nolondon <- update(mod, 
+              data=subset(fulldata, !(asscentre %in% 
+                 c("Hounslow","Croydon","Barts")))) |> 
+  ci.exp(subset="pm25_07", ctr.mat=matrix(10))
+
+#FIT MODEL WITHOUT STRATA(CENTRE) WITHOUT LONDON 
+fmod <- funfmod(strata=c("sex","birthyear"), 
+                conf=confall)
+resens$nocentrelondon <- update(mod, fmod, 
+                data=subset(fulldata, !(asscentre %in% 
+                      c("Hounslow","Croydon","Barts")))) |> 
+  ci.exp(subset="pm25_07", ctr.mat=matrix(10))
+
+
+# FIT MODEL WITH STRATA(CENTRE) WITHOUT GLASGOW
+fmod <-  funfmod()
+resens$noglasgow <- update(mod, data=subset(fulldata, 
+                  !(asscentre %in% c("Glasgow")))) |> 
+  ci.exp(subset="pm25_07", ctr.mat=matrix(10))
+
+#FIT MODEL WITHOUT STRATA(CENTRE) WITHOUT GLASGOW
+fmod <- funfmod(strata=c("sex","birthyear"), 
+                conf=confall)
+resens$nocentreglasgow <- update(mod, fmod,
+              data=subset(fulldata,
+                          !(asscentre %in% c("Glasgow")))) |> 
+  ci.exp(subset="pm25_07", ctr.mat=matrix(10))
+
+
+# FIT MODEL WITH STRATA(CENTRE) WITHOUT LONDON AND GLASGOW
+fmod <-  funfmod()
+resens$nolondglas <- update(mod, data=subset(fulldata, 
+                    !(asscentre %in% c("Hounslow","Croydon","Barts","Glasgow")))) |> 
+  ci.exp(subset="pm25_07", ctr.mat=matrix(10))
+
+#FIT MODEL WITHOUT STRATA(CENTRE) WITHOUT LONDON AND GLASGOW
+fmod <- funfmod(strata=c("sex","birthyear"), 
+                conf=confall)
+resens$nocentrelondglas <- update(mod, fmod,
+                          data=subset(fulldata,
+                      !(asscentre %in% c("Hounslow","Croydon","Barts","Glasgow")))) |> 
+  ci.exp(subset="pm25_07", ctr.mat=matrix(10))
+
+
+
+
+
+
